520c0998a89b4ede8f6abdc8fb48f024bbf82601
Mabelys Arias <MabelysA@gmail.com>
2021-08-04 15:25:16 -0400

    creates User model

---
+++ db/migrate/20210804192026_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email_address
+      t.string :phone_number
+      t.string :password_digest
+      t.boolean :admin
+
+      t.timestamps
+    end
+  end
+end
