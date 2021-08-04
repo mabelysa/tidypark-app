26c48b67ccadaf90a58a63276767933d1fc1c484
Mabelys Arias <MabelysA@gmail.com>
2021-08-04 16:46:52 -0400

    removes admin column, readds admin column wiwith boolean datatype

---
+++ db/migrate/20210804202031_delete_admin_from_user.rb
@@ -0,0 +1,5 @@
+class DeleteAdminFromUser < ActiveRecord::Migration[6.1]
+  def change
+    remove_column :users, :admin
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_04_192026) do
+ActiveRecord::Schema.define(version: 2021_08_04_202456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,9 +21,9 @@ ActiveRecord::Schema.define(version: 2021_08_04_192026) do
     t.string "email_address"
     t.string "phone_number"
     t.string "password_digest"
-    t.boolean "admin"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false, null: false
   end
 
 end
