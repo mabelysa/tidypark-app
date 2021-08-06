be678c5b70a26142ebb014bc78937b55e8c42b64
Mabelys Arias <MabelysA@gmail.com>
2021-08-06 15:23:23 -0400

    creates parks controller methods

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,2 +1,51 @@
 class ParksController < ApplicationController
+  before_action :authenticate_admin, except: [:index, :show]
+
+  def index
+    parks = Park.all
+    render json: parks.as_json
+  end
+
+  def create
+    parks = Park.new(
+      name: params["name"],
+      borough: params["borough"],
+      address: params["address"],
+      size: params["size"],
+      image_url: params["image_url"],
+    )
+    if parks.save
+      render json: parks.as_json
+    else
+      render json: { errors: parks.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    parks_id = params[:id]
+    parks = Park.find(parks_id)
+    render json: parks.as_json
+  end
+
+  def update
+    parks_id = params[:id]
+    parks = Park.find_by(id: parks_id)
+    parks.name = params["name"] || parks.name
+    parks.borough = params["borough"] || parks.borough
+    parks.address = params["address"] || parks.address
+    parks.size = params["size"] || parks.size
+    parks.image_url = params["image_url"] || parks.image_url
+    if parks.save
+      render json: parks.as_json
+    else
+      render json: { errors: parks.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    parks_id = params[:id]
+    parks = Movie.find_by(id: parks_id)
+    parks.destroy
+    render json: { message: "You have deleted the park!" }
+  end
 end
