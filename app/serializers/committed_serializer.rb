85b28f7b0edda78b93e2edbac9f1cbdf6ae9c3e6
Mabelys Arias <MabelysA@gmail.com>
2021-08-17 12:25:08 -0400

    modifies committed serializer

--- app/serializers/committed_serializer.rb
+++ app/serializers/committed_serializer.rb
@@ -1,3 +1,3 @@
 class CommittedSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :park_id, :observations, :needs, :teamsize, :tentativedate, :experience, :reason
+  attributes :id, :user_id, :park_id, :observations, :needs, :teamsize, :tentativedate, :experience, :reason, :park
 end
