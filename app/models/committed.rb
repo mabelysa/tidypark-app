34f08e531e1662f649cb45339c6ee3d6186bc611
Mabelys Arias <MabelysA@gmail.com>
2021-08-25 14:22:18 -0400

    updates tentativedate in committed model page

--- app/models/committed.rb
+++ app/models/committed.rb
@@ -2,7 +2,7 @@ class Committed < ApplicationRecord
   belongs_to :park
   belongs_to :user
 
-  validates_format_of :tentativedate, :with => /\d{2}\/\d{2}\/\d{4}/, :message => "^Date must be in the following format: mm/dd/yyyy"
+  validates_format_of :tentativedate, :with => /\d{2}\/\d{2}\/\d{4}/, :message => "must be in the following format: mm/dd/yyyy"
 
   # def dateformat
   #   tentativedate.strftime("%b %d, %Y")
