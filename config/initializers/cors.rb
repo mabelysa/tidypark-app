bd4627fda01710315398386a9a80a16f5207b48c
Mabelys Arias <MabelysA@gmail.com>
2021-09-07 17:48:34 -0400

    changes netlify name in rack-cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "example.com", "localhost:8080", "tidypark.netlify.app"
+    origins "example.com", "localhost:8080", "https://tidypark.netlify.app"
 
     resource "*",
       headers: :any,
